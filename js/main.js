c7866c5d43ad8a07a3999a9bf09e06c8fd6e5ac2
Verónica Reina <veroreinah@gmail.com>
2018-06-28 17:20:09 +0200

    Fix

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 window.onload = function() {
-  var canvas = document.getElementById('canvas');
-  var startBtn = document.getElementById('start-button');
-  var header = document.getElementsByTagName('header')[0];
+  var canvas = document.getElementById("canvas");
+  var startBtn = document.getElementById("start-button");
+  var header = document.getElementsByTagName("header")[0];
   var game;
   var requestAnimationId;
 
-  var mobileControls = document.getElementsByClassName('mobile-controls')[0];
-  var mobileBtns = document.getElementsByClassName('btn-mobile');
-  var isTouchDevice = 'ontouchstart' in document.documentElement;
+  var mobileControls = document.getElementsByClassName("mobile-controls")[0];
+  var mobileBtns = document.getElementsByClassName("btn-mobile");
+  var isTouchDevice = "ontouchstart" in document.documentElement;
 
   loadSounds();
 
@@ -16,19 +16,19 @@ window.onload = function() {
 
     if (isTouchDevice) {
       btn.ontouchstart = function() {
-        createEvents(this, 'onmobiledown');
+        createEvents(this, "onmobiledown");
       };
-  
+
       btn.ontouchend = function() {
-        createEvents(this, 'onmobileup');
+        createEvents(this, "onmobileup");
       };
     } else {
       btn.onmousedown = function() {
-        createEvents(this, 'onmobiledown');
+        createEvents(this, "onmobiledown");
       };
-  
+
       btn.onmouseup = function() {
-        createEvents(this, 'onmobileup');
+        createEvents(this, "onmobileup");
       };
     }
   }
@@ -36,14 +36,14 @@ window.onload = function() {
   startBtn.onclick = function() {
     startGame();
 
-    header.classList.add('small');
-    mobileControls.classList.add('active');
-    canvas.classList.add('mobile-version');
+    header.classList.add("small");
+    mobileControls.classList.add("active");
+    canvas.classList.add("mobile-version");
   };
 
   function startGame() {
     game = new Game(canvas);
-    
+
     updateCanvas(-1);
   }
 
@@ -56,17 +56,17 @@ window.onload = function() {
     } else {
       window.cancelAnimationFrame(requestAnimationId);
 
-      startBtn.innerHTML = 'Play Again';
-      header.classList.remove('small');
-      mobileControls.classList.remove('active');
-      canvas.classList.remove('mobile-version');
+      startBtn.innerHTML = "Play Again";
+      header.classList.remove("small");
+      mobileControls.classList.remove("active");
+      canvas.classList.remove("mobile-version");
     }
   }
 
   function createEvents(btn, eventName) {
-    var direction = btn.getAttribute('data-direction');
+    var direction = btn.getAttribute("data-direction");
     var event = new CustomEvent(eventName, {
-      'detail': { 'direction': direction }
+      detail: { direction: direction }
     });
 
     document.dispatchEvent(event);
@@ -74,16 +74,16 @@ window.onload = function() {
 
   function loadSounds() {
     if (!createjs.Sound.initializeDefaultPlugins()) {
-			return;
-		}
-
+      return;
+    }
+    
     var assetsPath = "./audio/";
-		var sounds = [
-			{src: "cat-meowing.mp3", id: 'cat'},
-			{src: "chimes.mp3", id: 'yarn'},
-			{src: "chimes.mp3", id: 'fishbone'}
+    var sounds = [
+      { src: "cat-meowing.mp3", id: "cat" },
+      { src: "chimes.mp3", id: "yarn" },
+      { src: "chimes.mp3", id: "fishbone" }
     ];
-
-		createjs.Sound.registerSounds(sounds, assetsPath);
+    
+    createjs.Sound.registerSounds(sounds, assetsPath);
   }
 };
